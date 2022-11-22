9e2b61bc2958c45c3dc5e05f19badb465ee27430
jvesguerra <jvesguerra@up.edu.ph>
2022-11-22 17:00:56 +0800

    Feat: Authentication

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:week7_networking_discussion/providers/auth_provider.dart';
+import 'package:email_validator/email_validator.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -11,53 +14,97 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     TextEditingController firstNameController = TextEditingController();
     TextEditingController lasttNameController = TextEditingController();
 
-    final email = TextField(
+    bool _validateEmail = false;
+    bool _validatePassword = false;
+    bool _validateFirstName = false;
+    bool _validateLastName = false;
+
+    final email = TextFormField(
       controller: emailController,
       decoration: const InputDecoration(
         hintText: "Email",
       ),
+      validator: (email) {
+        if (email != null && email.length == 0) {
+          return 'Email required';
+        } else if (email != null && !EmailValidator.validate(email)) {
+          return 'Email a valid email';
+        } else {
+          return null;
+        }
+      },
     );
 
     //First name
-    final firstName = TextField(
-      controller: firstNameController,
-      decoration: const InputDecoration(
-        hintText: "First Name",
-      ),
-    );
+    final firstName = TextFormField(
+        controller: firstNameController,
+        decoration: InputDecoration(
+          hintText: "First Name",
+        ),
+        validator: (value) {
+          if (value != null && value.length == 0) {
+            return 'First Name Required';
+          } else {
+            return null;
+          }
+        });
     //Last name
-    final lastName = TextField(
-      controller: lasttNameController,
-      decoration: const InputDecoration(
-        hintText: "Last Name",
-      ),
-    );
+    final lastName = TextFormField(
+        autofocus: true,
+        controller: lasttNameController,
+        decoration: InputDecoration(
+          hintText: "Last Name",
+        ),
+        validator: (value) {
+          if (value != null && value.length == 0) {
+            return 'Last Name Required';
+          } else {
+            return null;
+          }
+        });
 
-    final password = TextField(
-      controller: passwordController,
-      obscureText: true,
-      decoration: const InputDecoration(
-        hintText: 'Password',
-      ),
-    );
+    final password = TextFormField(
+        controller: passwordController,
+        obscureText: true,
+        decoration: const InputDecoration(
+          hintText: 'Password',
+        ),
+        validator: (value) {
+          if (value != null && value.length == 0) {
+            return 'Password required';
+          } else if (value != null && value.length < 6) {
+            return 'Enter valid password';
+          } else {
+            return null;
+          }
+        });
 
     final SignupButton = Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: ElevatedButton(
         onPressed: () {
-          context.read<AuthProvider>().signUp(
-              emailController.text,
-              passwordController.text,
-              firstNameController.text,
-              lasttNameController.text);
-          Navigator.pop(context);
+          final isValidForm = formKey.currentState!.validate();
+
+          if (isValidForm) {
+            context.read<AuthProvider>().signUp(
+                emailController.text,
+                passwordController.text,
+                firstNameController.text,
+                lasttNameController.text);
+            Navigator.pop(context);
+          }
         },
-        child: const Text('Sign up', style: TextStyle(color: Colors.white)),
+        child: const Text(
+          'Sign up',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
 
@@ -73,23 +120,27 @@ class _SignupPageState extends State<SignupPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.only(left: 40.0, right: 40.0),
-          children: <Widget>[
-            const Text(
-              "Sign Up",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25),
-            ),
-            email,
-            firstName,
-            lastName,
-            password,
-            SignupButton,
-            backButton
-          ],
+      body: Form(
+        autovalidateMode: AutovalidateMode.disabled,
+        key: formKey,
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+            children: <Widget>[
+              const Text(
+                "Sign Up",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              ),
+              email,
+              firstName,
+              lastName,
+              password,
+              SignupButton,
+              backButton
+            ],
+          ),
         ),
       ),
     );
